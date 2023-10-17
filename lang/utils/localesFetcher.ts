bd998071b84bbc974a31d51afa481d44cef17ad5
Andrea Basile <andrea.basile@kampaay.com>
2023-10-17 22:31:44 +0200

    refactor: cleanup comments

--- lang/utils/localesFetcher.ts
+++ lang/utils/localesFetcher.ts
@@ -1,12 +1,4 @@
 const I18N_BASE_URL = "https://i18n.staging.kampaay.com/";
-// export default defineI18nLocale(async (locale) => {
-//   try {
-//     return await (await fetch(`${I18N_BASE_URL}fe-${locale}.json`)).json();
-//   } catch (e) {
-//     console.error(e);
-//     return {};
-//   }
-// });
 
 export const localesFetcher = async (locale: string) => {
   try {
